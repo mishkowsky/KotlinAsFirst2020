d8d36e01bdac02f726be8354a8e258338f8f4bf0
mishkowsky <aleksandrov.m2542@gmail.com>
2020-09-29 17:34:15 +0300

    29.09

--- src/lesson3/task1/Loop.kt
+++ src/lesson3/task1/Loop.kt
@@ -99,14 +99,28 @@ fun fib(n: Int): Int {
  *
  * Для заданного числа n > 1 найти минимальный делитель, превышающий 1
  */
-fun minDivisor(n: Int): Int = TODO()
+fun minDivisor(n: Int): Int {
+    for (i in 2..sqrt(n.toDouble()).toInt()) {
+        if ((n % i) == 0) {
+            return i
+        }
+    }
+    return n
+}
 
 /**
  * Простая (2 балла)
  *
  * Для заданного числа n > 1 найти максимальный делитель, меньший n
  */
-fun maxDivisor(n: Int): Int = TODO()
+fun maxDivisor(n: Int): Int {
+    for (i in 2..sqrt(n.toDouble()).toInt()) {
+        if ((n % i) == 0) {
+            return n / i
+        }
+    }
+    return 1
+}
 
 /**
  * Простая (2 балла)
@@ -202,13 +216,13 @@ fun sin(x: Double, eps: Double): Double = TODO()
  * Подумайте, как добиться более быстрой сходимости ряда при больших значениях x.
  * Использовать kotlin.math.cos и другие стандартные реализации функции косинуса в этой задаче запрещается.
  */
-fun cos(x: Double, eps: Double): Double {
+fun cos(x: Double, eps: Double): Double = TODO() /*{
     var i = 0
     var result = 1
     do {
         result = (- x.pow(i)).toInt()
     } while ()
-}
+}*/
 
 /**
  * Сложная (4 балла)
