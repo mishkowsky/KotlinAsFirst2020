8bd5ad7ee42496e7235ddcfc0ef5d393fc425860
mishkowsky <aleksandrov.m2542@gmail.com>
2020-11-24 22:48:49 +0300

    24.11

--- test/lesson6/task1/Tests.kt
+++ test/lesson6/task1/Tests.kt
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertThrows
 import org.junit.jupiter.api.Tag
 import org.junit.jupiter.api.Test
-import java.util.*
 
 class Tests {
     @Test
